b04259c50029327ddd7a6114a88e0ea7bb079467
danielmartinsteix-dot <danielmartinsteix@gmail.com>
2026-07-06 12:17:25 -0300

    Adiciona suporte ao Render

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,15 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./chave-firebase.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = require("./chave-firebase.json");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
